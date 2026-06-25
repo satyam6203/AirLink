a2f26b36456f1a0b776f0fc44656009952469f20
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-06-25 17:41:59 +0530

    start working on airline core service

---
+++ Microservices/services/airline-core-service/src/test/java/com/airlink/airline_core_service/AirlineCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlink.airline_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
