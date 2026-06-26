29fddf6777420e784f820c2183ba68e4085d5672
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-06-26 16:36:42 +0530

    airline service completed and all apis are tested

--- Microservices/services/airline-core-service/src/main/java/com/airlink/airline_core_service/Controller/AirLineController.java
+++ Microservices/services/airline-core-service/src/main/java/com/airlink/airline_core_service/Controller/AirLineController.java
@@ -1,17 +1,22 @@
 package com.airlink.airline_core_service.Controller;
 
-import com.airlink.airline_core_service.Model.Airline;
 import com.airlink.airline_core_service.Service.AirLineService;
 import enums.AirLineStatus;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import payload.request.AirLineRequest;
+import payload.response.AirLineDropdownItem;
 import payload.response.AirLineResponse;
 
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/airline")
+@RequestMapping("/api/airlines")
 @RequiredArgsConstructor
 public class AirLineController {
 
@@ -20,15 +25,15 @@ public class AirLineController {
     @PostMapping("/create/{ownerId}")
     public ResponseEntity<AirLineResponse> createAirline(
             @Valid @RequestBody AirLineRequest request,
-            @PathVariable Long ownerId
+            @RequestHeader("X-User-id") Long userId
     ){
-        AirLineResponse airline = airLineService.createAirLine(request, ownerId);
+        AirLineResponse airline = airLineService.createAirLine(request, userId);
         return ResponseEntity.ok(airline);
     }
 
-    @GetMapping("/owner/{ownerId}")
+    @GetMapping("/admin")
     public ResponseEntity<AirLineResponse> getAirLineByOwner(
-            @PathVariable Long ownerId
+            @RequestHeader("X-User-id") Long ownerId
     ) throws Exception {
         AirLineResponse id = airLineService.getAirLineByOwner(ownerId);
         return ResponseEntity.ok(id);
@@ -50,18 +55,21 @@ public class AirLineController {
         AirLineResponse response = airLineService.updateAirLine(request, ownerId);
         return ResponseEntity.ok(response);
     }
-//
-//    @GetMapping("/all")
-//    public ResponseEntity<AirLineResponse> allAirLines(
-//
-//    )
+
+    // Controller
+    @GetMapping("/all")
+    public ResponseEntity<Page<AirLineResponse>> allAirLines(Pageable pageable) {
+        return ResponseEntity.ok(
+                airLineService.getAllAirLines(pageable)
+        );
+    }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteAirLine(
             @PathVariable Long id,
-            @PathVariable Long ownerId
+            @RequestHeader("X-User-id") Long userId
     ) throws Exception {
-        airLineService.deleteAirLine(id, ownerId);
+        airLineService.deleteAirLine(id, userId);
         return ResponseEntity.ok("Airline Deleted SuccessFully");
     }
 
@@ -72,5 +80,39 @@ public class AirLineController {
             ) throws Exception {
         AirLineResponse response = airLineService.changeStatus(airlineId, status);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/dropdown")
+    public ResponseEntity<List<AirLineDropdownItem>> getAirLinesForDropDown(){
+        return ResponseEntity.ok(airLineService.getAirLineDropDown());
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<AirLineResponse> updateAirLine(
+            @Valid @RequestBody AirLineRequest request,
+            @RequestHeader("X-User-Id") Long userId
+    ) throws Exception {
+        return ResponseEntity.ok(airLineService.updateAirLine(request, userId));
+    }
+
+    @PostMapping("/{id}/approve")
+    public ResponseEntity<AirLineResponse> approveAirline(
+            @PathVariable Long id
+    ) throws Exception {
+        return ResponseEntity.ok(airLineService.changeStatus(id,AirLineStatus.ACTIVE ));
+    }
+
+    @PostMapping("/{id}/suspend")
+    public ResponseEntity<AirLineResponse> suspendAirline(
+            @PathVariable Long id
+    ) throws Exception {
+        return ResponseEntity.ok(airLineService.changeStatus(id,AirLineStatus.INACTIVE));
+    }
+
+    @PostMapping("/{id}/ban")
+    public ResponseEntity<AirLineResponse> BanAirline(
+            @PathVariable Long id
+    ) throws Exception {
+        return ResponseEntity.ok(airLineService.changeStatus(id,AirLineStatus.BANNED ));
     }
 }
