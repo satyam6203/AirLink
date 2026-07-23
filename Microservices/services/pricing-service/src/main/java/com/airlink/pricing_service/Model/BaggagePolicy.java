cfac419de1fe2b09095752bd23d7a31ada18e677
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-07-23 09:01:42 +0530

    bagger policy entity completed

--- Microservices/services/pricing-service/src/main/java/com/airlink/pricing_service/Model/BaggagePolicy.java
+++ Microservices/services/pricing-service/src/main/java/com/airlink/pricing_service/Model/BaggagePolicy.java
@@ -2,7 +2,11 @@ package com.airlink.pricing_service.Model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
 
 @Entity
 @Getter
@@ -17,5 +21,38 @@ public class BaggagePolicy {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private Fare fare;
+
+    private String name;
+
+    private String description;
+
+    private Double cabinBaggageMaxWeight;
+
+    private Integer cabinBaggagePrice = 1;
+
+    private Double cabinBaggageWeightPerPrice;
+
+    private Double cabinBaggageMaxDimension;
+
+    private Double checkInBaggageMaxWeight;
+
+    private Integer checkInBaggagePrice = 1;
+
+    private Double checkInBaggageWeightPerPrice;
+
+    private Integer freeCheckedBagsAllowance = 0;
+
+    private Boolean priorityBaggage = false;
+
+    private Boolean extraBaggageAllowance = false;
+
+    private Long airlineId;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
 
 }
