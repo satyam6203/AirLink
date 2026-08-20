25edd0857fc3b9a0846b6275501e3c4b7f250e0c
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-08-20 23:06:15 +0530

    api gateway is compeleted

--- Microservices/cloud/api-gateway/src/main/java/com/AirLine/Config/RouteConfig.java
+++ Microservices/cloud/api-gateway/src/main/java/com/AirLine/Config/RouteConfig.java
@@ -1,4 +1,134 @@
 package com.AirLine.Config;
 
+import org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.function.*;
+
+import javax.print.attribute.standard.Severity;
+
+@Configuration
 public class RouteConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> authRoutes(){
+        return GatewayRouterFunctions.route("auth-routes")
+                .route(RequestPredicates.path("/auth/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("user-service"))
+                .build();
+    }
+
+    @Bean
+    @Order(1)
+    public RouterFunction<ServerResponse> adminLocationServiceRoutes() {
+        return GatewayRouterFunctions.route("admin-location-routes")
+                .route(RequestPredicates.POST("/api/cities/**"), HandlerFunctions.http())
+                .route(RequestPredicates.POST("/api/airports/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("location-service"))
+//                .filter(CircuitBreakerFilterFunctions.circuitBreaker("location-service-cb", URI.create("forward:/fallback")))
+//                .before(this::jwtAuthFilter)
+//                .before(request -> requireRole(request, "ROLE_SYSTEM_ADMIN"))
+                .build();
+    }
+
+    @Bean
+    @Order(1)
+    public RouterFunction<ServerResponse> adminAirlineCoreServiceRoutes(){
+        return GatewayRouterFunctions.route("admin-airline-core-routes")
+                .route(RequestPredicates.GET("/api/airlines"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("airline-core-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoutes(){
+        return GatewayRouterFunctions.route("user-service-route")
+                .route(RequestPredicates.path("/api/user/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("user-service"))
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    public RouterFunction<ServerResponse> airlineCoreServiceRoutes(){
+        return GatewayRouterFunctions.route("airline-core-routes")
+                .route(RequestPredicates.path("/api/airlines/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/aircraft/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("airline-core-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> seatServiceRoutes() {
+        return GatewayRouterFunctions.route("seat-service-routes")
+                .route(RequestPredicates.path("/api/cabin-classes/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/seat-maps/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/seats/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/seat-instances/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/flight-instance-cabins/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("seat-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> flightOpsServiceRoutes() {
+        return GatewayRouterFunctions.route("flight-ops-routes")
+                .route(RequestPredicates.path("/api/flights/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/flight-instances/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/flight-schedules/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("flight-ops-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> pricingServiceRoutes() {
+        return GatewayRouterFunctions.route("pricing-service-routes")
+                .route(RequestPredicates.path("/api/fares/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/fare-rules/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/baggage-policies/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("pricing-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> AncillaryServiceRoutes() {
+        return GatewayRouterFunctions.route("ancillary-service-routes")
+                .route(RequestPredicates.path("/api/meals/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/ancillaries/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/insurance-coverages/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/flight-meals/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/flight-cabin-ancillaries/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("ancillary-service"))
+
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    public RouterFunction<ServerResponse> locationServiceRoutes() {
+        return GatewayRouterFunctions.route("location-service-routes")
+                .route(RequestPredicates.path("/api/cities/**"), HandlerFunctions.http())
+                .route(RequestPredicates.path("/api/airports/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("location-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> bookingServiceRoutes() {
+        return GatewayRouterFunctions.route("booking-service-routes")
+                .route(RequestPredicates.path("/api/bookings/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("booking-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> paymentServiceRoutes() {
+        return GatewayRouterFunctions.route("payment-service-routes")
+                .route(RequestPredicates.path("/api/payments/**"), HandlerFunctions.http())
+                .filter(LoadBalancerFilterFunctions.lb("payment-service"))
+                .build();
+    }
 }
